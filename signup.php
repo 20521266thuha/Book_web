58febff3df0dc8342fca10af9d635cbe6fbbd6dd
MSI\Mia <02thuhabui@gmail.com>
2023-11-21 21:44:57 +0700

    no mesasgge

---
+++ signup.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SIGN UP - MIA BOOKCORNER</title>
+    <link rel="stylesheet" href="./Style/index.css"> 
+</head>
+
+<body>
+    <div id="login-container" style=" width: 100%; height: 100%">
+        <div class="container-content">
+            <div class="container-content-left">
+                <img src="./Assets/LoginBox.png"/>
+            </div>
+            <div class="container-content-right">
+                <p class="title" style="color: #046543; font-size: 20px; margin-top: 20px;"><b>WELCOME TO MIA BOOKCORNER!</b></p>
+                <p class="title" style="color: #9ba9a4; font-size: 14px">Create your account</p>
+                <p class="title" style="color: #046543;margin-top: 10px; font-size: 16px"><b><i>Phone number (*)</i></b></p>
+                <input style="width: 323px; height: 17px" />
+                <p class="title" style="color: #046543;margin-top: 10px; font-size: 16px"><b><i>Name (*)</i></b></p>
+                <input style="width: 323px; height: 17px" />
+                <p class="title" style="color: #046543;margin-top: 10px; font-size: 16px"><b><i>Password (*)</i></b></p>
+                <input style="width: 323px; height: 17px" />
+                <p class="title" style="color: #046543;margin-top: 10px; font-size: 16px"><b><i>Confirm password (*)</i></b></p>
+                <input style="width: 323px; height: 17px" />
+                <button class="button" style="margin-top: 30px;"><p><b>SIGN UP</b></p></button>
+                <p class="title" style="color: #9ba9a4; margin-top: 10px; margin-left:60px; font-style: italic; font-size: 14px">Already have an account? <a href="index.php">Login here</a> </p>
+                <p class="title" style="color: #9ba9a4; font-size: 8px; margin-left: 113px; margin-top:15px;"><i>Copyright © MiaBookcorner.com</i></p>
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
