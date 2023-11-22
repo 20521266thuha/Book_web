ed9ff7bdecce8d6b7e054c7ada85d5307a7443bb
MSI\Mia <02thuhabui@gmail.com>
2023-11-22 14:38:30 +0700

    hihi

--- signup.php
+++ signup.php
@@ -1,35 +1,84 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SIGN UP - MIA BOOKCORNER</title>
-    <link rel="stylesheet" href="./Style/index.css"> 
+	<title>SIGN UP - MIA BOOKCORNER</title>
+	<link rel="stylesheet" type="text/css" href="./Style/index.css">
 </head>
-
 <body>
-    <div id="login-container" style=" width: 100%; height: 100%">
+     <form action="signup-check.php" method="post">
+     <?php if (isset($_GET['error'])) { ?>
+     		<p class="error"><?php echo $_GET['error']; ?></p>
+     	<?php } ?>
+
+          <?php if (isset($_GET['success'])) { ?>
+               <p class="success"><?php echo $_GET['success']; ?></p>
+          <?php } ?>
+
+     <div id="login-container" style=" width: 100%; height: 100%">
         <div class="container-content">
             <div class="container-content-left">
                 <img src="./Assets/LoginBox.png"/>
             </div>
             <div class="container-content-right">
+                <!-- tieu de -->
                 <p class="title" style="color: #046543; font-size: 20px; margin-top: 20px;"><b>WELCOME TO MIA BOOKCORNER!</b></p>
                 <p class="title" style="color: #9ba9a4; font-size: 14px">Create your account</p>
-                <p class="title" style="color: #046543;margin-top: 10px; font-size: 16px"><b><i>Phone number (*)</i></b></p>
-                <input style="width: 323px; height: 17px" />
-                <p class="title" style="color: #046543;margin-top: 10px; font-size: 16px"><b><i>Name (*)</i></b></p>
-                <input style="width: 323px; height: 17px" />
-                <p class="title" style="color: #046543;margin-top: 10px; font-size: 16px"><b><i>Password (*)</i></b></p>
-                <input style="width: 323px; height: 17px" />
-                <p class="title" style="color: #046543;margin-top: 10px; font-size: 16px"><b><i>Confirm password (*)</i></b></p>
-                <input style="width: 323px; height: 17px" />
-                <button class="button" style="margin-top: 30px;"><p><b>SIGN UP</b></p></button>
-                <p class="title" style="color: #9ba9a4; margin-top: 10px; margin-left:60px; font-style: italic; font-size: 14px">Already have an account? <a href="index.php">Login here</a> </p>
+
+                <!-- Phone -->
+                <p class="title" style="color: #046543; font-size: 16px; margin-top: 10px;"><b><i>Phone number (*)</i></b></p>
+                <?php if (isset($_GET['uname'])) { ?>
+                <input style="width: 323px; height: 17px"
+                      type="text" 
+                      name="uname" 
+                      placeholder="Phone number"
+                      value="<?php echo $_GET['uname']; ?>"><br>
+                <?php }else{ ?>
+                <input style="width: 323px; height: 17px" 
+                      type="text" 
+                      name="uname" 
+                      placeholder="Phone number"><br>
+                <?php }?>
+
+                <!-- Name -->
+                <p class="title" style="color: #046543; font-size: 16px"><b><i>Name (*)</i></b></p>
+                <?php if (isset($_GET['name'])) { ?>
+                <input style="width: 323px; height: 17px"
+                      type="text" 
+                      name="name" 
+                      placeholder="Name"
+                      value="<?php echo $_GET['name']; ?>"><br>
+                <?php }else{ ?>
+                <input style="width: 323px; height: 17px" 
+                      type="text" 
+                      name="name" 
+                      placeholder="Name"><br>
+                <?php }?>
+
+                    <!-- Password -->
+                <p class="title" style="color: #046543; font-size: 16px"><b><i>Password (*)</i></b></p>
+                <input 
+                    style="width: 323px; height: 17px"
+                    type="password" 
+                    name="password" 
+                    placeholder="Password"><br>
+
+                    <!-- Re-pasword -->
+                <p class="title" style="color: #046543; font-size: 16px"><b><i>Confirm password (*)</i></b></p>
+                <input style="width: 323px; height: 17px" 
+                 type="password" 
+                 name="re_password" 
+                 placeholder="Confirm password"><br>
+
+                    <!-- Button -->
+                <button type="submit" class="button" style="margin-top: 20px;"><p><b>SIGN UP</b></p></button>
+
+
+                <p class="title" style="color: #9ba9a4; margin-top: 10px; margin-left:60px; font-style: italic; font-size: 14px">Already have an account? <a href="index.php" class="ca">Login here</a> </p>
                 <p class="title" style="color: #9ba9a4; font-size: 8px; margin-left: 113px; margin-top:15px;"><i>Copyright © MiaBookcorner.com</i></p>
 
             </div>
         </div>
     </div>
-</body>
-</html>
+     	
+
+
